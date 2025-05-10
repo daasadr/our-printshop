414564ad2a35fe7a883476874212f74aa974eeda
Dagmar Drbálková <daasa.d@seznam.cz>
2025-05-10 10:31:23 +0200

    o10

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import { compare } from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/lib/prisma";
