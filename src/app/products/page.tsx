7a128f10b538816ec1fbed06ac238eb377a12c57
Dagmar Drbálková <daasa.d@seznam.cz>
2025-05-16 22:20:10 +0200

    konečně5?

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { Suspense } from 'react';
 import { ProductList } from '@/components/ProductList';
 import { ProductListSkeleton } from '@/components/ProductListSkeleton';
