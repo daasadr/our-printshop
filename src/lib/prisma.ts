4a3bc8e832c536565a363f61d635f7814e7794a1
Dagmar Drbálková <daasa.d@seznam.cz>
2025-05-23 15:31:15 +0200

    l1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
-let prisma;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
 
-module.exports = prisma; 
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma; 
